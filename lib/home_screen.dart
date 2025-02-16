90f6c7be6e36ffa07be93b6153de574a06879d7c
mhamad <ranjamolaq@gmail.com>
2025-02-16 00:38:01 -0800

    t

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -641,7 +641,7 @@ class _TextToSpeechScreenState extends State<TextToSpeechScreen> {
                     ElevatedButton.icon(
                       onPressed: _saveToHistory,
                       icon: const Icon(Icons.save, color: Colors.white),
-                      label: const Text("Save gg",
+                      label: const Text("Save",
                           style: TextStyle(color: Colors.white)),
                       style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
                     ),
